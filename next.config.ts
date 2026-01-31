f5fe5b7d744e72ea83bab733cf17ed9c7ee04814
herdirudian <herdirudian07@gmail.com>
2026-02-01 00:42:05 +0700

    Fix Sentry config: Merge options into single argument for v8+ compatibility

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: "family-the-lodge",
   project: "javascript-nextjs",
-}, {
+
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
