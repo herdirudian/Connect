673ce09ff34b1b8a0b8f00a5d24fb51ef80510b2
herdirudian <herdirudian07@gmail.com>
2026-02-01 00:45:41 +0700

    Fix build: Remove unsupported Sentry options hideSourceMaps and widenClientFileUpload

--- next.config.ts
+++ next.config.ts
@@ -28,12 +28,6 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
 });
